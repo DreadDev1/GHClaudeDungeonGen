ef6e69756ec2bf9fb75d37c5dfb98107484f319b
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-12-06 21:04:13 -0500

    added DungeonGenLibrary

---
+++ Source/GHClaudeDungeonGen/Public/Libraries/DungeonGenLibrary.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DungeonGenLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHCLAUDEDUNGEONGEN_API UDungeonGenLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
