1c845434bcd2d03de0ee88e4d91411df794bc3e2
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-12-06 22:26:26 -0500

    Add DebugHelpers Component

---
+++ Source/GHClaudeDungeonGen/Private/Debugging/DebugHelpers.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Debugging/DebugHelpers.h"
+
+UDebugHelpers::UDebugHelpers()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+#if WITH_EDITOR
+void UDebugHelpers::DrawGridCreated(const FVector& CellCenter, float CellSize, float Duration)
+{
+	if (!bShowGridCreated) return;
+	DrawCellBox(CellCenter, CellSize, FColor::Green, Duration);
+}
+
+void UDebugHelpers::DrawUnoccupiedCell(const FVector& CellCenter, float CellSize, float Duration)
+{
+	if (!bShowUnoccupiedCells) return;
+	DrawCellBox(CellCenter, CellSize, FColor::Blue, Duration);
+}
+
+void UDebugHelpers::DrawOccupiedCell(const FVector& CellCenter, float CellSize, float Duration)
+{
+	if (!bShowOccupiedCells) return;
+	DrawCellBox(CellCenter, CellSize, FColor::Red, Duration);
+}
+
+void UDebugHelpers::DrawCellBox(const FVector& CellCenter, float CellSize, const FColor& Color, float Duration)
+{
+	if (!GetWorld()) return;
+
+	const float HalfSize = CellSize * 0.5f;
+	const FVector Extent(HalfSize, HalfSize, 1.0f); // Flat box for floor grid
+
+	DrawDebugBox(
+		GetWorld(),
+		CellCenter,
+		Extent,
+		Color,
+		false,      // bPersistent
+		Duration,
+		0,          // DepthPriority
+		2.0f        // Thickness
+	);
+}
+#endif
---
+++ Source/GHClaudeDungeonGen/Public/Debugging/DebugHelpers.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DebugHelpers.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GHCLAUDEDUNGEONGEN_API UDebugHelpers : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UDebugHelpers();
+
+#if WITH_EDITOR
+	// Toggle bools for debug visualization
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug|Grid")
+	bool bShowGridCreated = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug|Grid")
+	bool bShowUnoccupiedCells = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug|Grid")
+	bool bShowOccupiedCells = true;
+
+	// Draw grid outline when grid is created (Green)
+	UFUNCTION(BlueprintCallable, Category = "Debug|Grid")
+	void DrawGridCreated(const FVector& CellCenter, float CellSize, float Duration = 2.0f);
+
+	// Draw unoccupied cell outline (Blue)
+	UFUNCTION(BlueprintCallable, Category = "Debug|Grid")
+	void DrawUnoccupiedCell(const FVector& CellCenter, float CellSize, float Duration = 2.0f);
+
+	// Draw occupied cell outline (Red)
+	UFUNCTION(BlueprintCallable, Category = "Debug|Grid")
+	void DrawOccupiedCell(const FVector& CellCenter, float CellSize, float Duration = 2.0f);
+
+private:
+	void DrawCellBox(const FVector& CellCenter, float CellSize, const FColor& Color, float Duration);
+#endif
+};
